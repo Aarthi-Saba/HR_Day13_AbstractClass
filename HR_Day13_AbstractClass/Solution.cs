fe7b0e523cf47a0df5b295f5c8d6114cbb489a48
aarthi <aarthi.saba@gmail.com>
2020-05-19 17:39:53 -0400

    Add project files.

---
+++ HR_Day13_AbstractClass/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HR_Day13_AbstractClass
+{
+    abstract class Book
+    {
+        protected String title;
+        protected String author;
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+    }
+
+    //Write MyBook class
+    class MyBook : Book
+    {
+        protected int myBookPrice;
+
+        public MyBook(String bookTitle, String bookAuthor,int bookPrice) : base(bookTitle,bookAuthor)
+        {
+            myBookPrice = bookPrice;
+
+        }
+        public override void display()
+        {
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Author: {author}");
+            Console.WriteLine($"Price: {myBookPrice}");
+        }
+    }
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+}
